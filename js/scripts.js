5022b63d2df5c11514c3d0bf4ae0ae9f13fba958
Student <example@example.com>
2019-10-23 10:28:35 -0700

    update html, jQuery functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
 
+  $("form#shout-input").submit(function(event) {
+    var loud = $("input#uppercase").val().toUpperCase();
+
+    $("p.uppercase").text(loud);
+    $("#uppercase").val(" ");
+    event.preventDefault();
+  });
+
+  $('button#clear').click(function() {
+      // $("div#Shout").reload(true);
+  });
+
   $("form#letter-input").submit(function(event) {
     var info = $("input#name").val();
 
+    $("#name").val(" ");
+
     $(".name").text(info);
     $('#letter').show();
-    // info.clear();
 
     event.preventDefault();
-
   });
+
 
 });
